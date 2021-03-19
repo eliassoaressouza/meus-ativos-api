65c9b8aee4e795c1e9a0a8ed4d5e65358de64982
Elias Soares <eliassoaressouza@gmail.com>
2021-03-19 19:18:48 -0300

    ajustes

--- src/service/usuario/usuario.service.ts
+++ src/service/usuario/usuario.service.ts
@@ -2,7 +2,7 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { UsuarioDocument, UsuarioModel } from 'src/models/usuario.model';
-import { obterAmbiente } from 'src/app.module';
+
 
 
 @Injectable()
@@ -22,8 +22,6 @@ export class UsuarioService {
     console.log('logado########################################')
     console.log(email)
     console.log(senha)
-
-    console.log(obterAmbiente())
     console.log('logado########################################')
     const usuario =await this.usuarioModel.find({email,senha}).exec();
     return usuario;
