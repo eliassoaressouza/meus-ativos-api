cca9ad26825e23946bdfaade7364eea2e8548bc0
Elias Soares <eliassoaressouza@gmail.com>
2021-03-25 18:48:44 -0300

    ajustes

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -9,19 +9,29 @@ import { UsuarioService } from 'src/service/usuario/usuario.service';
 import { AtivoModel, AtivosSchema } from 'src/models/ativo.model';
 import { AuthModule } from 'src/auth/auth.module';
 import { ClassificacaoService } from 'src/service/classificacao/classificacao.service';
-import { ClassificacaoModel, ClassificacaoSchema } from 'src/models/Classificacao.model';
+import {
+  ClassificacaoModel,
+  ClassificacaoSchema,
+} from 'src/models/Classificacao.model';
 import { ClassificacaoController } from './classificacao.controller';
-
-
-
+import { ClassificacaoModule } from 'src/service/classificacao/classificacao.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: UsuarioModel.name, schema: UsuarioSchema }]),
-  MongooseModule.forFeature([{ name: AtivoModel.name, schema: AtivosSchema }]),
-  MongooseModule.forFeature([{ name: ClassificacaoModel.name, schema: ClassificacaoSchema }]),
-  AuthModule],
-  
-  providers: [UsuarioService,AtivoService,ClassificacaoService],
-  controllers: [UsuarioController, AtivoController,ClassificacaoController]
+  imports: [
+    ClassificacaoModule,
+    MongooseModule.forFeature([
+      { name: UsuarioModel.name, schema: UsuarioSchema },
+    ]),
+    MongooseModule.forFeature([
+      { name: AtivoModel.name, schema: AtivosSchema },
+    ]),
+    MongooseModule.forFeature([
+      { name: ClassificacaoModel.name, schema: ClassificacaoSchema },
+    ]),
+    AuthModule,
+  ],
+
+  providers: [UsuarioService, AtivoService, ClassificacaoService],
+  controllers: [UsuarioController, AtivoController, ClassificacaoController],
 })
 export class ApiModule {}
